f697bbdb819da37179666847ef338364698bc769
PNZStudio <insensak.007@gmail.com>
2021-03-02 21:10:39 +0700

    Update footer.php

--- views/body/footer.php
+++ views/body/footer.php
@@ -1,5 +1,7 @@
 <center class="mt-3 mb-3">
-    Created by <i class="fas fa-heart"></i> <a taget="_blank" class="text-dark" href="https://facebook.com/phaiwan.nakrub"><u>PhaiwanStudio</u></a>
+    Created by <i class="fas fa-heart"></i> 
+    <a target="_blank" class="text-dark" href="https://facebook.com/phaiwan.nakrub"><u>PhaiwanStudio</u></a><br>
+    <a target="_blank" class="text-dark" href="https://github.com/PNZStudio/PNZ-CTF"><u><i class="fab fa-github"></i> GitHub/PNZ-CTF</u></a>
 </center>
 
 <script type="text/javascript" src="/views/assets/js/jquery.min.js"></script>
